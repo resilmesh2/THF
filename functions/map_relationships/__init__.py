5f290c0b1d4a9ac399651d7c4cc3747c5d4c5b50
Stephen Jacob <stephen.jacob@tus.ie>
2025-08-21 12:11:26 +0100

    Merged access_patterns into behavioural_corelation activity

--- functions/map_relationships/__init__.py
+++ functions/map_relationships/__init__.py
@@ -3,11 +3,9 @@ Relationship mapping functions
 """
 
 from .entity_to_entity import execute as entity_to_entity_execute
-from .access_patterns import execute as access_patterns_execute
-from .activity_correlation import execute as activity_correlation_execute
+from .behavioural_correlation import execute as behavioural_correlation_execute
 
 __all__ = [
     "entity_to_entity_execute",
-    "access_patterns_execute", 
-    "activity_correlation_execute"
+    "behavioural_correlation_execute"
 ]
